95e7500c9d1029dd6745cb2dd29ea42153971f6a
Aqib Ansari <aquib@kotharitech.in>
2023-03-19 19:38:21 +0530

    get all subscriptions

---
+++ app/controllers/subscriptions.js
@@ -0,0 +1,106 @@
+const model = require('../models/city')
+const { matchedData } = require('express-validator')
+const utils = require('../middleware/utils')
+const db = require('../middleware/db')
+let successData = {message: 'Success', totalRecord: 0, data: [], status: 200}
+
+/*********************
+ * Private functions *
+ *********************/
+
+/**
+ * Checks if a city already exists excluding itself
+ * @param {string} id - id of item
+ * @param {string} name - name of item
+ */
+const cityExistsExcludingItself = async (id, name) => {
+  return new Promise((resolve, reject) => {
+    model.findOne(
+      {
+        name,
+        _id: {
+          $ne: id
+        }
+      },
+      (err, item) => {
+        utils.itemAlreadyExists(err, item, reject, 'CITY_ALREADY_EXISTS')
+        resolve(false)
+      }
+    )
+  })
+}
+
+/**
+ * Checks if a city already exists in database
+ * @param {string} name - name of item
+ */
+const cityExists = async (name) => {
+  return new Promise((resolve, reject) => {
+    model.findOne(
+      {
+        name
+      },
+      (err, item) => {
+        utils.itemAlreadyExists(err, item, reject, 'CITY_ALREADY_EXISTS')
+        resolve(false)
+      }
+    )
+  })
+}
+
+/**
+ * Gets all items from database
+ */
+const getAllItemsFromDB = async () => {
+  return new Promise((resolve, reject) => {
+    model.find(
+      {},
+      '-updatedAt -createdAt',
+      {
+        sort: {
+          name: 1
+        }
+      },
+      (err, items) => {
+        if (err) {
+          reject(utils.buildErrObject(422, err.message))
+        }
+        resolve(items)
+      }
+    )
+  })
+}
+
+/********************
+ * Public functions *
+ ********************/
+
+/**
+ * Get all items function called by route
+ * @param {Object} req - request object
+ * @param {Object} res - response object
+ */
+exports.getAllItems = async (req, res) => {
+  try {
+    res.status(200).json(await getAllItemsFromDB())
+  } catch (error) {
+    utils.handleError(res, error)
+  }
+}
+
+/**
+ * Get items function called by route
+ * @param {Object} req - request object
+ * @param {Object} res - response object
+ */
+exports.getAllItems = async (req, res) => {
+  try {
+    const query = `select * from subscription_plan`;
+    let tempData = await utils.executeQuery(query);
+    successData.data = tempData;
+    successData.totalRecord = tempData.length;
+    res.status(200).json(successData);
+  } catch (error) {
+    utils.handleError(res, error)
+  }
+}
